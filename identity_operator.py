e7c00d1e17eec4508bf26bffca214e4c656d92d8
Mahak Preet kaur <mahakpreet65@gmail.com>
2024-08-13 01:08:20 +0530

    Add files via upload

---
+++ identity_operator.py
@@ -0,0 +1,13 @@
+# is
+x=5;
+if(type(x) is int ):
+    print("true");
+else:
+    print("False");
+
+# is not
+x=8.9;
+if(type(x) is not int ):
+    print("true");
+else:
+    print("False");
---
+++ membership_op.py
@@ -0,0 +1,22 @@
+# in
+a=11;
+list={1,2,4,5,7};
+if(a in list):
+    print("yes");
+else:
+    print("no");
+
+a=1;
+list={1,2,4,5,7};
+if(a in list):
+    print("yes");
+else:
+    print("no");
+# not in
+a=11;
+list={1,2,4,5,7};
+if(a not in list):
+    print("yes");
+else:
+    print("no");
+
