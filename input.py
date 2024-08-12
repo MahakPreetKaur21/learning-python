7adb326039de3dab854b18cd191de68c23f3924f
Mahak Preet kaur <mahakpreet65@gmail.com>
2024-08-12 21:40:06 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,9 @@
+a=int(input("Enter value of a:"));
+b=int(input("Enter value of b:"));
+# c=int(input("Enter value of c:"));
+c=a+b;
+print("Sum of",a,"and",b,"is",c);2
+
+#input name
+Name=(input("Enter your Name:"));
+print("Welcone Mr/ms",Name);
